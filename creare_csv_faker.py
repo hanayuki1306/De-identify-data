6bdb423f42079a977ea67a42b87de4d6e39a081c
Nguyen Phi Son <hanayuki1306@gmail.com>
2019-06-04 17:07:31 +0700

    chinh_sua_syntax

--- creare_csv_faker.py
+++ creare_csv_faker.py
@@ -3,12 +3,16 @@ from faker  import Faker
 fake = Faker()
 
 lookup={}
+root_file ='2010BA_DEIDENT_INTHB0837I_286230_10160249.csv'
+write_out_file = open('test_output.csv', 'w')
+with open (root_file,'r') as csv_file:
 
-son_write = open('test_output.csv', 'w')
-with open ('2010BA_DEIDENT_INTHB0837I_286230_10160249.csv','r') as csv_file:
-
+    # headers ={
+    #     'filename','transaction_loop','line_no,sha256_hash','entity_identifier_code','entity_type_qualifier','subscriber_last_name','subscriber_first_name','subscriber_middle_name_or_initial','identification_code_qualifier','subscriber_primary_identifier','subscriber_address_line','subscriber_address_line','subscriber_city_name','subscriber_state_code','subscriber_postal_zone_or_zip_code','location_identifier','date_time_period_format_qualifier','subscriber_birth_date','subscriber_gender_code','reference_identification_qualifier','subscriber_supplemental_identifier','subscriber_name_suffix'
+    # }
     spamreader = csv.DictReader(csv_file)
-    my_write = csv.DictWriter(son_write,fieldnames=spamreader.fieldnames) 
+    my_write = csv.DictWriter(write_out_file,fieldnames=spamreader.fieldnames) 
+    my_write.writeheader()
     
     for row in spamreader:
 
@@ -44,4 +48,4 @@ with open ('2010BA_DEIDENT_INTHB0837I_286230_10160249.csv','r') as csv_file:
             }
         my_write.writerow(row)
     
-son_write.close()
+write_out_file.close()
